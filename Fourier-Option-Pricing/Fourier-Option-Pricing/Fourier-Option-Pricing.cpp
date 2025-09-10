89b1646fc56edc11f31ab50495562244fe2ba9c9
SamyakLuitel <samkotaku@gmail.com>
2025-09-10 12:17:03 +0100

    project setup

---
+++ Fourier-Option-Pricing/Fourier-Option-Pricing/Fourier-Option-Pricing.cpp
@@ -0,0 +1,12 @@
+﻿// Fourier-Option-Pricing.cpp : Defines the entry point for the application.
+//
+
+#include "Fourier-Option-Pricing.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Option Pricing Using Fourier Techniques" << endl;
+	return 0;
+}
